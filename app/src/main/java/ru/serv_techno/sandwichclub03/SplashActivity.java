69aa90a10ebcd2a6d471dc5b6492407f77de3c74
maximmo <morotkinmaxim@gmail.com>
2017-04-26 01:49:07 +0800

    qwe

--- app/src/main/java/ru/serv_techno/sandwichclub03/SplashActivity.java
+++ app/src/main/java/ru/serv_techno/sandwichclub03/SplashActivity.java
@@ -3,7 +3,12 @@ package ru.serv_techno.sandwichclub03;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private Retrofit retrofit;
+    private static APIv1 myApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
